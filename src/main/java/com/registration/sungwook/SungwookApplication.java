08c3e6ca9b6e3b1114b1b3978d43eff50fb7a19f
choisungwook <kgg1959@naver.com>
2020-09-03 03:55:01 +0900

    프로젝트 생성

---
+++ src/main/java/com/registration/sungwook/SungwookApplication.java
@@ -0,0 +1,13 @@
+package com.registration.sungwook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SungwookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SungwookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/registration/sungwook/SungwookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registration.sungwook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SungwookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
