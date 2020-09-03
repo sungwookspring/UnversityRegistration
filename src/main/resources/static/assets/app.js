761a7bc29f8243451f561286ccc20e07a56910af
choisungwook <kgg1959@naver.com>
2020-09-03 20:11:28 +0900

    Add 수강등록-뷰 연동

--- src/main/resources/static/assets/app.js
+++ src/main/resources/static/assets/app.js
@@ -4,6 +4,9 @@ var main = {
         $('#btn-student-register').on('click', function(){
             _this.student_register();
         });
+        $('#btn-course-enroll').on('click', function(){
+            _this.enroll_course();
+        });
     },
 
     student_register : function() {
@@ -14,6 +17,25 @@ var main = {
         $.ajax({
             type: 'POST',
             url: '/student/register',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function (){
+            alert('회원가입이 완료되었습니다');
+            window.location.href = '/';
+        }).fail(function (error){
+            alert(JSON.stringify(error));
+        });
+    },
+
+    enroll_course : function() {
+        var data = {
+            title: $('#course_title').val(),
+            grade: $('#course_last_name').val()
+        };
+        $.ajax({
+            type: 'POST',
+            url: '/professor/course/enroll',
             dataType: 'json',
             contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data)
