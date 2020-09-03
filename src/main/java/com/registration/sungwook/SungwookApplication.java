3b92459ba07d68e877f488e1921c22a35ecb5ee5
choisungwook <kgg1959@naver.com>
2020-09-03 16:48:24 +0900

    Add JAPAuditing on SpringBootApp

--- src/main/java/com/registration/sungwook/SungwookApplication.java
+++ src/main/java/com/registration/sungwook/SungwookApplication.java
@@ -1,13 +1,25 @@
 package com.registration.sungwook;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SungwookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SungwookApplication.class, args);
 	}
 
+//	@Bean
+//	public CommandLineRunner sampleData(ThemeParkRideRepository repository) {
+//		return (args) -> {
+//			repository.save(new ThemeParkRide("Rollercoaster", "Train ride that speeds you along.", 5, 3));
+//			repository.save(new ThemeParkRide("Log flume", "Boat ride with plenty of splashes.", 3, 2));
+//			repository.save(new ThemeParkRide("Teacups", "Spinning ride in a giant tea-cup.", 2, 4));
+//		};
+//	}
 }
